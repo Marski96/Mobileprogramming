14614958f8524fff4edd7e8dada8c6916dd6f2ee
Marski96 <jani.maaranen@saunalahti.fi>
2020-04-15 12:34:14 +0300

    currencies can be seen in dropdown

--- Exercise8-EuroConverter/App.js
+++ Exercise8-EuroConverter/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import EuroConverter from './components/EuroConverter';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <EuroConverter></EuroConverter>
     </View>
   );
 }
